5a56bf76115e8f3096d87798a723005f42ce8d35
Joshua Chumbley <jdc@Sudo.local>
2018-06-08 10:05:13 -0500

    Added random image generator and README

---
+++ README.md
@@ -0,0 +1,27 @@
+# Image Generators
+
+> Image generators for the See Anything project ([SeeAnything.org](http://seeanything.org/))
+
+## Background
+
+Start with a black image and increment each pixel until the image becomes white. Then you will have created every image possible within that frame. 
+
+The See Anything project is an exercise in (combinatorics)[https://en.wikipedia.org/wiki/Combinatorics] that aims to use different techniques to express as many images as possible through image generator algorithms.
+
+## Generators
+
+The current implementation has three image generators:
+
+* **Linear** - 320x240 raster with 8-bit greyscale pixels, linearly generating images by incrementing each pixel until the entire raster is filled. This technique will take trillions of years to complete. Number of possible images is 256<sup>76,800</sup>.
+
+* **Linear 1-Bit** 320x240 raster with 1-bit pixels, where each pixel is either black or white. Increments in the same way as the Linear Generator but will complete sooner as it has a reduced number set. Number of possible images is 2<sup>76,800</sup>.
+
+* **Random 8-bit**  320x240 raster with 8-bit greyscale pixels, with each pixel's value set to a pseudo-random number from 0 to 255. Images are not based on a particular set and may repeat. Number of possible images is 256<sup>76,800</sup>.
+
+## How it works
+
+The image generator code runs on a server, with each generator working on its own thread and producing one image per generator at a time. When a generator creates a new image, the previous image is overwritten. These images are served on the webpages at [SeeAnything.org](http://seeanything.org/).
+
+## Contributing
+
+Both the (SeeAnything website)[https://github.com/SeeAnything/seeanything.org] and the image generators are open for pull requests.
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 #[macro_use]
 extern crate bmp;
+extern crate rand;
 
 use std::fs;
 use std::path::Path;
 use std::vec::Vec;
 use std::thread;
-use std::time::{SystemTime, UNIX_EPOCH, Duration};
+use std::time::Duration;
 
 use bmp::{Image, Pixel};
+use rand::prelude::*;
 
 fn incr_vec_8bpp(px_vec: &mut Vec<u8>) {
 
@@ -113,16 +115,14 @@ fn generate_images_8bpp(w: u32, h: u32, iter: u32, fname: &str) {
 
     loop {
         // Create the image
-        let img = img_from_vec_mono(320, 240, &px);
+        let img = img_from_vec_mono(w, h, &px);
 
         // Write the image to a temporary file and rename it so the
         // server can find it. These steps are important in order to
         // avoid serving files that are in the process of being written
         let temp_fpath = format!("{}.tmp", fname);
         img.save(temp_fpath.to_string()).unwrap();
-        fs::rename(temp_fpath, fname);
-
-        println!("Saved image (8bpp)");
+        fs::rename(temp_fpath, fname).unwrap();
 
 
         // Increment the pixel vector
@@ -131,6 +131,7 @@ fn generate_images_8bpp(w: u32, h: u32, iter: u32, fname: &str) {
         while incr_number < iter {
             incr_vec_8bpp(&mut px);
             incr_number = incr_number + 1;
+            thread::sleep(Duration::from_millis(1));
         }
     }
 }
@@ -162,16 +163,14 @@ fn generate_images_1bpp(w: u32, h: u32, iter: u32, fname: &str) {
 
     loop {
         // Create the image
-        let img = img_from_vec_mono(320, 240, &px);
+        let img = img_from_vec_mono(w, h, &px);
 
         // Write the image to a temporary file and rename it so the
         // server can find it. These steps are important in order to
         // avoid serving files that are in the process of being written
         let temp_fpath = format!("{}.tmp", fname);
         img.save(temp_fpath.to_string()).unwrap();
-        fs::rename(temp_fpath, fname);
-
-        println!("Saved image (1bpp)");
+        fs::rename(temp_fpath, fname).unwrap();
 
 
         // Increment the pixel vector
@@ -180,18 +179,34 @@ fn generate_images_1bpp(w: u32, h: u32, iter: u32, fname: &str) {
         while incr_number < iter {
             incr_vec_1bpp(&mut px);
             incr_number = incr_number + 1;
+            thread::sleep(Duration::from_millis(1));
         }
     }
+}
+
+fn generate_images_rand(w: u32, h: u32, fname: &str) {
+    let mut img = Image::new(w, h);
+    let mut rng = thread_rng();
+
+    for (x, y) in img.coordinates() {
+        let c = rng.gen_range(0, 255);
+        img.set_pixel(x, y, px!(c, c, c));
+    }
+
+    let temp_fpath = format!("{}.tmp", fname);
+    img.save(temp_fpath.to_string()).unwrap();
+    fs::rename(temp_fpath, fname).unwrap();
 }
 
 fn main() {
 
     const WIDTH: u32 = 320;
     const HEIGHT: u32 = 240;
-    const INCREMENT: u32 = u32::max_value();
+    const INCREMENT: u32 = 500;
 
     const FPATH_8BPP: &str = "theimage.bmp";
     const FPATH_1BPP: &str = "theimage_1bpp.bmp";
+    const FPATH_RAND: &str = "theimage_rand.bmp";
 
     thread::spawn(move || {
         generate_images_8bpp(WIDTH, HEIGHT, INCREMENT, FPATH_8BPP);
@@ -200,10 +215,19 @@ fn main() {
     println!("Started 8bpp thread");
 
     thread::spawn(move || {
-        generate_images_1bpp(WIDTH, HEIGHT, 1, FPATH_1BPP);
+        generate_images_1bpp(WIDTH, HEIGHT, INCREMENT, FPATH_1BPP);
     });
 
     println!("Started 1bpp thread");
+
+    thread::spawn(move || {
+        loop {
+            generate_images_rand(WIDTH, HEIGHT, FPATH_RAND);
+            thread::sleep(Duration::from_millis(500));
+        }
+    });
+
+    println!("Started random thread");
 
     loop {
         let min = Duration::from_secs(60);
